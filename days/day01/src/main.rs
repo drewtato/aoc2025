9c4eae7f0f882c426a81b7dad38f30ee7d497ab1
Drewtato <15526875+drewtato@users.noreply.github.com>
2025-12-01 21:07:22 -0800

    Done with day 1

--- days/day01/src/main.rs
+++ days/day01/src/main.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-
 use helpers::*;
 
 fn main() {
@@ -11,10 +9,56 @@ struct Solver;
 
 impl solver_interface::ChildSolver for Solver {
     fn part_one(input: &[u8], _debug: u8) -> impl Display + 'static {
-        "unimplemented"
+        let mut input = Consume::new(input);
+        let mut zeros: usize = 0;
+        let mut dial: i32 = 50;
+
+        while !input.is_empty() {
+            let direction = match input.consume_byte().unwrap() {
+                b'L' => -1,
+                b'R' => 1,
+                _ => panic!("not L or R"),
+            };
+            let distance: i32 = input.int().unwrap();
+            dial += direction * distance;
+            dial = dial.rem_euclid(100);
+            if dial == 0 {
+                zeros += 1;
+            }
+            if !input.newline() {
+                break;
+            }
+        }
+
+        zeros
     }
 
     fn part_two(input: &[u8], _debug: u8) -> impl Display + 'static {
-        "unimplemented"
+        let mut input = Consume::new(input);
+        let mut zeros: usize = 0;
+        let mut dial: i32 = 50;
+
+        while !input.is_empty() {
+            let direction = match input.consume_byte().unwrap() {
+                b'L' => -1,
+                b'R' => 1,
+                _ => panic!("not L or R"),
+            };
+            let distance: i32 = input.int().unwrap();
+
+            for _ in 0..distance {
+                dial += direction;
+                dial = dial.rem_euclid(100);
+                if dial == 0 {
+                    zeros += 1;
+                }
+            }
+
+            if !input.newline() {
+                break;
+            }
+        }
+
+        zeros
     }
 }
