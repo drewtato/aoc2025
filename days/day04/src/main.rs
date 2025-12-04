d74d5dfe942034e7c9074bb82a592c6e431261f4
Drewtato <15526875+drewtato@users.noreply.github.com>
2025-12-03 23:13:26 -0800

    done with day 4

--- days/day04/src/main.rs
+++ days/day04/src/main.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-
 use helpers::*;
 
 fn main() {
@@ -11,10 +9,105 @@ struct Solver;
 
 impl solver_interface::ChildSolver for Solver {
     fn part_one(input: &[u8], _debug: u8) -> impl Display + 'static {
-        "unimplemented"
+        let grid = make_grid(input);
+        let width = grid[0].len();
+        let height = grid.len();
+        let mut accessible = 0;
+        for y in 0..height {
+            for x in 0..width {
+                if !*grid_get(&grid, [y, x]).unwrap() {
+                    continue;
+                }
+                let mut adjacent = 0;
+                neighbors(&grid, [y, x], |b| {
+                    if b {
+                        adjacent += 1
+                    }
+                });
+                if adjacent < 4 {
+                    accessible += 1;
+                }
+            }
+        }
+        accessible
     }
 
     fn part_two(input: &[u8], _debug: u8) -> impl Display + 'static {
-        "unimplemented"
+        let mut grid = make_grid(input);
+        let width = grid[0].len();
+        let height = grid.len();
+        let mut accessible = Vec::<[usize; 2]>::new();
+        let mut removed = 0;
+        loop {
+            for y in 0..height {
+                for x in 0..width {
+                    if !*grid_get(&grid, [y, x]).unwrap() {
+                        continue;
+                    }
+                    let mut adjacent = 0;
+                    neighbors(&grid, [y, x], |b| {
+                        if b {
+                            adjacent += 1
+                        }
+                    });
+                    if adjacent < 4 {
+                        accessible.push([y, x]);
+                    }
+                }
+            }
+
+            if accessible.is_empty() {
+                break;
+            }
+            removed += accessible.len();
+            for c in accessible.drain(..) {
+                *grid_get_mut(&mut grid, c).unwrap() = false;
+            }
+        }
+        removed
     }
+}
+
+fn make_grid(input: &[u8]) -> Vec<Vec<bool>> {
+    let mut input = Consume::new(input);
+    let mut grid = Vec::new();
+    while !input.is_empty() {
+        let mut line = input.next_newline();
+        line.split_off_last();
+        let row = line.iter().map(|&b| b == b'@').collect();
+        grid.push(row);
+    }
+    grid
+}
+
+fn neighbors(grid: &[Vec<bool>], coord: [usize; 2], mut f: impl FnMut(bool)) {
+    let [y, x] = coord;
+    for [dy, dx] in [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+    ] {
+        (|| {
+            let &b = grid_get(grid, [y.checked_add_signed(dy)?, x.checked_add_signed(dx)?])?;
+            f(b);
+            Some(())
+        })();
+    }
+}
+
+fn grid_get<T>(grid: &[Vec<T>], coord: [usize; 2]) -> Option<&T> {
+    let row = grid.get(coord[0])?;
+    let b = row.get(coord[1])?;
+    Some(b)
+}
+
+fn grid_get_mut<T>(grid: &mut [Vec<T>], coord: [usize; 2]) -> Option<&mut T> {
+    let row = grid.get_mut(coord[0])?;
+    let b = row.get_mut(coord[1])?;
+    Some(b)
 }
