16518de6ceac852070f16e977db4be408dc33cc5
Drewtato <15526875+drewtato@users.noreply.github.com>
2025-12-06 21:43:09 -0800

    better day 7

--- days/day07/src/main.rs
+++ days/day07/src/main.rs
@@ -9,48 +9,46 @@ struct Solver;
 
 impl solver_interface::ChildSolver for Solver {
     fn part_one(input: &[u8], _debug: u8) -> impl Display + 'static {
-        let start = input.find_byte(b'S').unwrap();
-        let width = input[start..].find_byte(b'\n').unwrap() + start + 1;
-        let mut beams: ArrayVec<bool, 200> = repeat_n_iter(false, width).collect();
-        beams[start] = true;
-        let mut splits = 0;
-        for row in input.chunks(width).skip(1) {
-            for (i, &space) in row.iter().enumerate() {
-                match space {
-                    b'.' | b'\n' => {}
-                    b'^' => {
-                        if beams[i] {
-                            splits += 1;
-                            beams[i] = false;
-                            beams[i - 1] = true;
-                            beams[i + 1] = true;
-                        }
-                    }
-                    _ => panic!("invalid space"),
-                }
+        let mut splits = 0usize;
+        solve(input, true, |i, beams| {
+            if beams[i] {
+                splits += 1;
+                beams[i] = false;
+                beams[i - 1] = true;
+                beams[i + 1] = true;
             }
-        }
+        });
         splits
     }
 
     fn part_two(input: &[u8], _debug: u8) -> impl Display + 'static {
-        let start = input.find_byte(b'S').unwrap();
-        let width = input[start..].find_byte(b'\n').unwrap() + start + 1;
-        let mut beams: ArrayVec<u64, 200> = repeat_n_iter(0, width).collect();
-        beams[start] = 1;
-        for row in input.chunks(width).skip(1) {
-            for (i, &space) in row.iter().enumerate() {
-                match space {
-                    b'.' | b'\n' => {}
-                    b'^' => {
-                        let n = replace(&mut beams[i], 0);
-                        beams[i - 1] += n;
-                        beams[i + 1] += n;
-                    }
-                    _ => panic!("invalid space"),
-                }
+        solve(input, 1, |i, beams| {
+            let n = replace(&mut beams[i], 0);
+            beams[i - 1] += n;
+            beams[i + 1] += n;
+        })
+        .into_iter()
+        .sum::<u64>()
+    }
+}
+
+fn solve<T: Default>(
+    input: &[u8],
+    init: T,
+    mut f: impl FnMut(usize, &mut [T]),
+) -> ArrayVec<T, 200> {
+    let start = input.find_byte(b'S').unwrap();
+    let width = input[start..].find_byte(b'\n').unwrap() + start + 1;
+    let mut beams: ArrayVec<_, 200> = repeat_with_iter(T::default).take(width).collect();
+    beams[start] = init;
+    for row in input.chunks(width).skip(1) {
+        for (i, &space) in row.iter().enumerate().take(width - 1) {
+            match space {
+                b'.' => {}
+                b'^' => f(i, &mut beams),
+                _ => panic!("invalid space"),
             }
         }
-        beams.into_iter().sum::<u64>()
     }
+    beams
 }
