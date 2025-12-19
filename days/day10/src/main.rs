d15428280de758d8c3b23efd6a51e7408affac29
Drewtato <15526875+drewtato@users.noreply.github.com>
2025-12-18 16:46:53 -0800

    another correct but slow

--- days/day10/src/main.rs
+++ days/day10/src/main.rs
@@ -1,7 +1,5 @@
-use std::sync::atomic::AtomicUsize;
-
 use helpers::*;
-use rayon::prelude::*;
+// use rayon::prelude::*;
 
 fn main() {
     use solver_interface::ChildSolverExt;
@@ -22,7 +20,8 @@ impl solver_interface::ChildSolver for Solver {
 
 fn lines(input: &[u8], f: impl Fn(Machine) -> u32 + Send + Sync) -> u32 {
     let mut input = Consume::new(input);
-    let mut machines = Vec::new();
+    // let mut machines = Vec::new();
+    let mut sum = 0;
     while !input.is_empty() {
         assert!(input.byte(b'['));
         let mut lights = 0;
@@ -67,35 +66,26 @@ fn lines(input: &[u8], f: impl Fn(Machine) -> u32 + Send + Sync) -> u32 {
         }
 
         assert!(input.newline());
-        machines.push(Machine {
+        let machine = Machine {
             lights,
             buttons,
             joltages,
-        });
+        };
+        // machines.push(machine);
+        sum += f(machine);
     }
-    let progress = AtomicUsize::new(0);
-    let total = machines.len();
-    machines
-        .into_par_iter()
-        .enumerate()
-        .map(|(i, machine)| {
-            let buttons = machine.buttons.len();
-            let presses = f(machine);
-            let current = progress.fetch_add(1, std::sync::atomic::Ordering::Relaxed) + 1;
-            eprintln!("done with {i} ({buttons} buttons) ({current}/{total})");
-            presses
-        })
-        .sum()
+
+    // machines.into_par_iter().map(f).sum()
+    sum
 }
 
 const MAX_BUTTONS: usize = 14;
 const MAX_JOLTAGES: usize = 10;
 
-#[allow(dead_code)]
 #[derive(Clone)]
 struct Machine {
     lights: StateInt,
-    buttons: ArrayVec<StateInt, MAX_BUTTONS>,
+    buttons: ArrayVec<ButtonInt, MAX_BUTTONS>,
     joltages: ArrayVec<u16, MAX_JOLTAGES>,
 }
 
@@ -128,60 +118,143 @@ impl Machine {
     }
 
     fn configure_joltage(self) -> u32 {
-        min_presses(
-            &mut self
+        macro_rules! specific {
+            ($($n:literal),* $(,)?) => {
+                match self.joltages.len() {
+                    $(
+                        $n => SpecificLengthMachine::<$n>::try_from(self)
+                            .unwrap()
+                            .configure_joltage(),
+                    )*
+                    n => panic!("please add length {n} to the match"),
+                }
+            };
+        }
+        specific!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct SpecificLengthMachine<const N: usize> {
+    buttons: ArrayVec<ButtonInt, MAX_BUTTONS>,
+    joltages: [u16; N],
+}
+
+impl<const N: usize> TryFrom<Machine> for SpecificLengthMachine<N> {
+    type Error = &'static str;
+
+    fn try_from(machine: Machine) -> Result<Self, Self::Error> {
+        Ok(Self {
+            buttons: machine.buttons,
+            joltages: *machine
                 .joltages
-                .iter()
-                .map(|&j| j as i16)
-                .collect::<ArrayVec<_, MAX_JOLTAGES>>(),
-            &self.buttons,
-        )
-        .unwrap()
+                .as_array()
+                .ok_or("wrong number of joltages")?,
+        })
     }
 }
 
-fn min_presses(joltages: &mut [i16], buttons: &[u16]) -> Option<u32> {
-    let Some((&button, rest_buttons)) = buttons.split_first() else {
-        if joltages.iter().all(|&j| j == 0) {
-            return Some(0);
-        } else {
-            return None;
-        }
-    };
-    let mut best_presses = None;
-    for presses in 0.. {
-        if joltages.iter().any(|j| j.is_negative()) {
-            unjolt(joltages, button, presses);
-            break;
-        }
-        if let Some(later) = min_presses(joltages, rest_buttons) {
-            best_presses = Some(
-                best_presses
-                    .unwrap_or_else(|| presses + later)
-                    .min(presses + later),
-            );
-        };
-        jolt(joltages, button);
-    }
-    best_presses
+#[derive(Debug)]
+struct State<const N: usize> {
+    heuristic_and_cost: u16,
+    cost: u16,
+    node: [u16; N],
+    last_button: u16,
 }
 
-fn jolt(joltages: &mut [i16], mut button: u16) {
-    for j in joltages.iter_mut().rev() {
+impl<const N: usize> PartialEq for State<N> {
+    fn eq(&self, other: &Self) -> bool {
+        self.heuristic_and_cost == other.heuristic_and_cost
+    }
+}
+
+impl<const N: usize> Eq for State<N> {}
+
+impl<const N: usize> PartialOrd for State<N> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<const N: usize> Ord for State<N> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.heuristic_and_cost.cmp(&other.heuristic_and_cost)
+    }
+}
+
+impl<const N: usize> SpecificLengthMachine<N> {
+    fn configure_joltage(self) -> u32 {
+        let mut queue = BinaryHeap::new_min();
+        let start = [0; N];
+        let cost = 0;
+        let heuristic = self.max_joltage_diff(start).unwrap();
+        queue.push(State {
+            heuristic_and_cost: heuristic + cost,
+            cost,
+            node: start,
+            last_button: 0,
+        });
+        let mut seen = HashSet::default();
+        seen.insert(start);
+
+        while let Some(State {
+            heuristic_and_cost,
+            cost,
+            node,
+            last_button,
+        }) = queue.pop()
+        {
+            seen.insert(node);
+            let new_cost = cost + 1;
+            let next_button = last_button as usize + 1;
+            if next_button < self.buttons.len() {
+                queue.push(State {
+                    heuristic_and_cost,
+                    cost,
+                    node,
+                    last_button: next_button as _,
+                });
+            }
+            let button = self.buttons[last_button as usize];
+            let new_node = jolt(node, button);
+            let Some(heuristic) = self.max_joltage_diff(new_node) else {
+                continue;
+            };
+            if heuristic == 0 {
+                return new_cost as _;
+            }
+            if seen.contains(&new_node) {
+                continue;
+            }
+            queue.push(State {
+                heuristic_and_cost: heuristic + cost,
+                cost: new_cost,
+                node: new_node,
+                last_button,
+            });
+        }
+
+        panic!("no path");
+    }
+
+    fn max_joltage_diff(&self, node: [u16; N]) -> Option<u16> {
+        let mut max = 0;
+        for (goal, n) in self.joltages.into_iter().zip(node) {
+            let diff = goal.checked_sub(n)?;
+            max = max.max(diff);
+        }
+        Some(max)
+    }
+}
+
+fn jolt<const N: usize>(mut node: [u16; N], mut button: u16) -> [u16; N] {
+    for n in node.iter_mut().rev() {
         if button.is_odd() {
-            *j -= 1;
+            *n += 1;
         }
         button /= 2;
     }
-}
-
-fn unjolt(joltages: &mut [i16], mut button: u16, presses: u32) {
-    for j in joltages.iter_mut().rev() {
-        if button.is_odd() {
-            *j += presses as i16;
-        }
-        button /= 2;
-    }
+    node
 }
 
 impl Debug for Machine {
